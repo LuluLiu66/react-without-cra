78f29656660c1e49150dd894b6edef66d88455d2
teresa-l <teresa.l@binance.com>
2023-07-14 19:46:50 +0800

    first commit: react app without cra

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello world</h1>
+        <hs>this is Lulu's own page</hs>
+      </div>
+    );
+  }
+}
+
+export default App;
