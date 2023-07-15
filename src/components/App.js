2561fae1703da2850d6c82b8939955c759ff043d
teresa-l <teresa.l@binance.com>
2023-07-15 11:41:42 +0800

    update UI

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { firestore } from "../firebase";
 import { addDoc, collection } from "@firebase/firestore";
 
 import {
+  Box,
   Button,
   Checkbox,
   Radio,
@@ -15,7 +16,6 @@ import {
   TextField,
 } from "@mui/material";
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
-import { async } from "@firebase/util";
 
 const dataStore = collection(firestore, "messages");
 
@@ -23,60 +23,87 @@ export default function App() {
   const [msg, setMsg] = useState("");
 
   return (
-    <div>
-      <h1>hello world</h1>
-      <h2>this is Lulu's website</h2>
-
-      <TextField
-        type="text"
-        value={msg}
-        onChange={(e) => {
-          setMsg(e.target.value);
-        }}
-      />
-      <Button
-        variant="contained"
-        onClick={async () => {
-          await addDoc(dataStore, { message: msg });
-          setTimeout(() => {
-            setMsg("");
-          }, 100);
+    <Box>
+      <Box
+        sx={{
+          fontSize: "18px",
+          textAlign: "center",
+          margin: "16px",
+          fontSize: "32px",
+          fontWeight: 800,
         }}
       >
-        submit
-      </Button>
+        this is Lulu's website
+      </Box>
 
-      <div>Button</div>
-      <Button variant="contained">Hello World</Button>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Box>
+          <TextField
+            type="text"
+            value={msg}
+            onChange={(e) => {
+              setMsg(e.target.value);
+            }}
+          />
+          <Button
+            sx={{ display: "block", marginBottom: "16px" }}
+            variant="contained"
+            onClick={async () => {
+              await addDoc(dataStore, { message: msg });
+              setTimeout(() => {
+                setMsg("");
+              }, 100);
+            }}
+          >
+            submit
+          </Button>
 
-      <div>Checkbox</div>
-      <div>
-        <Checkbox defaultChecked />
-        <Checkbox />
-        <Checkbox disabled />
-        <Checkbox disabled checked />
-      </div>
+          <Box>Checkbox</Box>
+          <Box sx={{ display: "block", marginBottom: "16px" }}>
+            <Checkbox defaultChecked />
+            <Checkbox />
+            <Checkbox disabled />
+            <Checkbox disabled checked />
+          </Box>
 
-      <div>Radio</div>
-      <FormControl>
-        <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
-        <RadioGroup
-          aria-labelledby="demo-radio-buttons-group-label"
-          defaultValue="female"
-          name="radio-buttons-group"
-        >
-          <FormControlLabel value="female" control={<Radio />} label="Female" />
-          <FormControlLabel value="male" control={<Radio />} label="Male" />
-          <FormControlLabel value="other" control={<Radio />} label="Other" />
-        </RadioGroup>
-      </FormControl>
+          <Box>Radio</Box>
+          <Box sx={{ display: "block", marginBottom: "16px" }}>
+            <FormControl>
+              <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+              <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="female"
+                name="radio-buttons-group"
+              >
+                <FormControlLabel
+                  value="female"
+                  control={<Radio />}
+                  label="Female"
+                />
+                <FormControlLabel
+                  value="male"
+                  control={<Radio />}
+                  label="Male"
+                />
+                <FormControlLabel
+                  value="other"
+                  control={<Radio />}
+                  label="Other"
+                />
+              </RadioGroup>
+            </FormControl>
+          </Box>
 
-      <div>tooltip</div>
-      <Tooltip title="Delete">
-        <IconButton>
-          <AccessAlarmIcon />
-        </IconButton>
-      </Tooltip>
-    </div>
+          <Box>tooltip</Box>
+          <Box sx={{ display: "block", marginBottom: "16px" }}>
+            <Tooltip title="Delete">
+              <IconButton>
+                <AccessAlarmIcon />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 }
